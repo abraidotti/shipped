ad1989b0a2fb75a363a4277b1f4eb83a2633d3e3
George Donald <dongsy326@gmail.com>
2017-11-28 16:23:30 -0500

    add new jobs and new boat

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,14 +1,32 @@
 class BoatsController < ApplicationController
   before_action :authenticate_user!
   def index
+    @boats = Boat.where(user_id: current_user.id)
   end
 
   def new
+    @boat = Boat.new
   end
 
   def edit
   end
 
   def show
+    @boat = Boat.find(params[:id])
   end
+
+  def create
+    @boat = current_user.boats.new(boat_params)
+    if @boat.save
+      redirect_to boats_path
+    else
+      redirect_to new_boat_path, notice: "An error occured while creating the new boat"
+    end
+  end
+
+  private
+  def boat_params
+    params.require(:boat).permit(:name, :container_amount, :location)
+  end
+
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,6 @@
 class Job < ApplicationRecord
+  validates_length_of :description, minimum: 20, too_short: "please enter at least 20 characters"
+  validates :cost, numericality: {greater_than: 999.99}
   has_many :boats
   has_many :users, through: :boats
 end
