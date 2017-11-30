e805f03e26f03241804e70d941a4fca246d27ba0
George Donald <dongsy326@gmail.com>
2017-11-30 15:28:28 -0500

    fixed conflicts in jobs.rb

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,17 +1,7 @@
 class Job < ApplicationRecord
   validates_length_of :description, minimum: 20, too_short: "please enter at least 20 characters"
   validates :cost, numericality: {greater_than: 999.99}
-  validates :origin_diff_from_dest
   has_and_belongs_to_many :boats
   has_many :users, through: :boats
-
-<<<<<<< HEAD
-  def origin_diff_from_dest
-    if self.origin == self.destination
-      errors.add("The destination cannot be the same as the origin.")
-    end
-  end
-=======
   validates :origin, uniqueness: { scope: :destination, message: "Origin and destination should be different." }
->>>>>>> e5b508d1df7735c64481f1915a6810d973f1b89f
 end
