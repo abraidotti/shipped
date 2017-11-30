2437bd8809fe84f5c540d8567307bafa3c527b3c
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-11-30 18:08:21 -0500

    add new boat validations

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,6 @@
 class Boat < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :container_amount, presence: true, numericality: { greater_than: 49}
   belongs_to :user
   has_and_belongs_to_many :jobs
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
